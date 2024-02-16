08a5e991466a4c50b5e258fbf080ca571d6fd412
Dan <98782899+danpowellb@users.noreply.github.com>
2024-02-16 23:19:41 +0000

    Update GRPCClientService.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -55,10 +55,10 @@ public class GRPCClientService {
 			.setA01(2)
 			.setA10(5)
 			.setA11(6)
-			.setB00(1)
-			.setB01(2)
-			.setB10(5)
-			.setB11(6)
+			.setB00(4)
+			.setB01(6)
+			.setB10(12)
+			.setB11(14)
 			.build());
 		String resp= A.getC00()+A.getC01()+A.getC10()+A.getC11()+"";
 		return resp;
