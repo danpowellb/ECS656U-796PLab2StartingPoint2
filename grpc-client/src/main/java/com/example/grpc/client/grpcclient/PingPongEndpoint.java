70a356518485a914cc10ddfda115d9a0232bdc29
Dan <98782899+danpowellb@users.noreply.github.com>
2024-02-16 16:41:54 +0000

    Update PingPongEndpoint.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
@@ -28,4 +28,8 @@ public class PingPongEndpoint {
 	public String add() {
 		return grpcClientService.add();
 	}
+	@GetMapping("/multiply")
+	public String multiply() {
+		return grpcClientService.multiply();
+	}
 }
